56fccdc07bf3349a6b96c893985642744112e5ed
manoelfilho <manoel.tsf@gmail.com>
2022-11-10 17:56:49 -0300

    removing user page

--- Conta/View/Configurations/Controllers/ConfigurationsController.swift
+++ Conta/View/Configurations/Controllers/ConfigurationsController.swift
@@ -4,7 +4,6 @@ import UIKit
 class ConfigurationsController: UIViewController{
     
     private lazy var optionsTable: [String:String] = [
-        "person":"configurations_personal_data",
         "wallet.pass":"configurations_account",
         "tag":"configurations_categories",
         "gear":"configurations_import"
@@ -15,7 +14,7 @@ class ConfigurationsController: UIViewController{
         headerView.backgroundColor = UIColor(named: K.colorBG1)
         let headerStackView = UIStackView()
         let nameLabel: UILabel = .textLabel(text: "configurations_title_page".localized(), fontSize: 30, color: .white, type: .Semibold)
-        let userImage = UIImageView(image: UIImage(named: "user"))
+        let userImage = UIImageView(image: UIImage(named: "icon_gear"))
         headerStackView.addArrangedSubview(nameLabel)
         headerStackView.addArrangedSubview(userImage)
         headerView.addSubview(headerStackView)
@@ -46,6 +45,7 @@ class ConfigurationsController: UIViewController{
     }()
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         
         view.backgroundColor = UIColor(named: K.colorBG1)
@@ -58,12 +58,32 @@ class ConfigurationsController: UIViewController{
         footerView.addSubview(labelFooter)
         view.addSubview(footerView)
         
-        headerView.fill(top: view.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor, padding: .init(top: 120, left: 0, bottom: 0, right: 0))
-        tableOptions.fill(top: headerView.bottomAnchor, leading: view.leadingAnchor, bottom: nil, trailing: view.trailingAnchor)
-        footerView.fill(top: tableOptions.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        headerView.fill(
+            top: view.topAnchor,
+            leading: view.leadingAnchor,
+            bottom: nil,
+            trailing: view.trailingAnchor,
+            padding: .init(top: 120, left: 0, bottom: 0, right: 0)
+        )
+        
+        tableOptions.fill(
+            top: headerView.bottomAnchor,
+            leading: view.leadingAnchor,
+            bottom: nil,
+            trailing: view.trailingAnchor
+        )
+        
+        footerView.fill(
+            top: tableOptions.bottomAnchor,
+            leading: view.leadingAnchor,
+            bottom: view.bottomAnchor,
+            trailing: view.trailingAnchor
+        )
+        
         labelFooter.fillSuperview(padding: .init(top: 0, left: 25, bottom: 25, right: 25))
         
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         tabBarController?.tabBar.isHidden = false
     }
@@ -75,17 +95,13 @@ extension ConfigurationsController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         switch indexPath.row {
-        case 0:
-            let authRegister = AuthRegisterController()
-            authRegister.modalTransitionStyle = .coverVertical
-            self.navigationController?.pushViewController(authRegister, animated: true)
             
-        case 1:
+        case 0:
             let accountsController = AccountsController()
             accountsController.modalTransitionStyle = .coverVertical
             self.navigationController?.pushViewController(accountsController, animated: true)
             
-        case 2:
+        case 1:
             let categoriesController = CategoriesController()
             categoriesController.modalTransitionStyle = .coverVertical
             self.navigationController?.pushViewController(categoriesController, animated: true)
@@ -105,12 +121,10 @@ extension ConfigurationsController: UITableViewDelegate, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: ConfigurationCell.cellConfiguration, for: indexPath) as! ConfigurationCell
         switch indexPath.row {
             case 0:
-                cell.configCell = ConfigCell(title: self.optionsTable["person"]?.localized(), symbolName: "person.fill", color: "#3ED598")
-            case 1:
                 cell.configCell = ConfigCell(title: self.optionsTable["wallet.pass"]?.localized(), symbolName: "wallet.pass.fill", color: "#FF565E")
-            case 2:
+            case 1:
                 cell.configCell = ConfigCell(title: self.optionsTable["tag"]?.localized(), symbolName: "tag.fill", color: "#FFC542")
-            case 3:
+            case 2:
                 cell.configCell = ConfigCell(title: self.optionsTable["gear"]?.localized(), symbolName: "gear", color: "#005DF2")
             default:
                 cell.configCell = ConfigCell(title: self.optionsTable["person"]?.localized(), symbolName: "person", color: "#FF565E")
