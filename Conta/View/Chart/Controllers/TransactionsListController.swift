867e84585babc381966ab89755a28edaf117f379
manoelfilho <manoel.tsf@gmail.com>
2022-11-11 18:47:39 -0300

    ...

--- Conta/View/Chart/Controllers/TransactionsListController.swift
+++ Conta/View/Chart/Controllers/TransactionsListController.swift
@@ -1,12 +1,22 @@
 import Foundation
 import UIKit
 
-class TransactionsListController: UITableViewController {
+class TransactionsListController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var titlePage: String?
     var transactions: [Transaction]?
     
     var data: [Transaction] = []
+    
+    private lazy var tableView: UITableView = {
+        let tableTransactions: UITableView = UITableView()
+        tableTransactions.separatorInset = .init(top: 0, left: 70, bottom: 0, right: 0)
+        tableTransactions.register(TransactionCell.self, forCellReuseIdentifier: TransactionCell.cellTransaction)
+        tableTransactions.backgroundColor = UIColor(named: K.colorBG1)
+        tableTransactions.showsVerticalScrollIndicator = false
+        tableTransactions.separatorColor = UIColor(named: K.colorText)?.withAlphaComponent(0.3)
+        return tableTransactions
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,17 +35,20 @@ class TransactionsListController: UITableViewController {
         tableView.dataSource = self
         tableView.delegate = self
         
+        tableView.frame = .init(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+        tableView.backgroundColor = .white
+        
+        view.addSubview(tableView)
+        
         loadData()
         
     }
     
     func loadData() {
         if let values = transactions {
-            print("passando aqui")
             DispatchQueue.main.async {
                 self.transactions = values
                 self.tableView.reloadData()
-                self.tableView.layoutIfNeeded()
             }
         }
     }
@@ -45,12 +58,20 @@ class TransactionsListController: UITableViewController {
 
 extension TransactionsListController {
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.data.count
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: TransactionCell.cellTransaction, for: indexPath) as! TransactionCell
+        cell.selectionStyle = .none
+        cell.transaction = self.data[indexPath.row]
+        return cell
+    }
+    
     
 }
