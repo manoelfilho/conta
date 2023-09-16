3f028f1176564b8250644ef9cef06d7b3219c6aa
manoelfilho <manoel.tsf@gmail.com>
2023-09-16 13:36:54 -0300

    Adicionando script para xcloud cloud - cocoapods dependencies

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,6 +3,22 @@
 cd ..
 
 echo ">>> SETUP ENVIRONMENT"
+echo 'export GEM_HOME=$HOME/gems' >>~/.bash_profile
+echo 'export PATH=$HOME/gems/bin:$PATH' >>~/.bash_profile
+export GEM_HOME=$HOME/gems
+export PATH="$GEM_HOME/bin:$PATH"
+
+echo ">>> UPDATE RubyGems"
+gem update --system
+
+echo ">>> INSTALL BUNDLER"
+gem install bundler --install-dir $GEM_HOME
+
+echo ">>> UPDATE BUNDLER"
+bundle update
+
+echo ">>> INSTALL DEPENDENCIES"
+bundle install
 
 echo ">>> INSTALL PODS"
 bundle exec pod install
